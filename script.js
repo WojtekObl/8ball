e7e3f0b23dab3771ec5b73b4b975ee6e9d39eb87
WojtekObl <wojtek.obloza@gmail.com>
2021-03-04 19:45:30 +0100

    Poprawione odpowiedzi

--- script.js
+++ script.js
@@ -3,34 +3,32 @@ const question = document.querySelector("input");
 const answer = document.querySelector(".answer");
 const error = document.querySelector(".error");
 
+const answers = [
+"Trudno powiedzieć", 
+"Może tak, może nie, nie wiem", 
+"Zdecydowanie tak", 
+"Zdecydowanie nie", 
+"Lepiej nie pytaj", 
+"Tak!", 
+"Nie!", 
+"Chyba tak", 
+"Chyba nie", 
+"Możliwe", 
+"Niemożliwe", 
+"Prawdopobne",
+"Prędzej zginiesz",
+"Raczej nie",
+"Oczywiście",
+"Nigdy w życiu",
+"***** ***",
+"Tak samo jak 70mln Sasina"
+];
+
 
 const getAnswer= () => {
 
-    const answers = 
-    [
-    "Trudno powiedzieć", 
-    "Może tak, może nie, nie wiem", 
-    "Zdecydowanie tak", 
-    "Zdecydowanie nie", 
-    "Lepiej nie pytaj", 
-    "Tak!", 
-    "Nie!", 
-    "Chyba tak", 
-    "Chyba nie", 
-    "Możliwe", 
-    "Niemożliwe", 
-    "Prawdopobne",
-    "Prędzej zginiesz",
-    "Raczej nie",
-    "Oczywiście",
-    "Nigdy w życiu",
-    "***** ***",
-    "Tak samo jak 70mln Sasina"
-    ];
-
-    const randomIndex = Math.floor(Math.random() * (answers.length-1))
-    
-    answer.textContent = answers[randomIndex];
+    const randomIndex = Math.floor(Math.random() * (answers.length-1));
+    answer.innerHTML = `<span>Odpowiedź: </span> ${answers[randomIndex]}`;
 
 }
 
@@ -39,8 +37,8 @@ const checkInput = () => {
 
     if (question.value.length <= 0) {
         error.textContent = "Musisz zadać pytanie"
-    } else if (question.value.charAt(question.value.length-1) != "?") {
-        error.textContent = "Pytanie musi kończyć się na '?' "
+    } else if (question.value.slice(-1) != "?") {
+        error.textContent = "Pytanie musi kończyć się znakiem '?' "
     } else {
         getAnswer()}
         
